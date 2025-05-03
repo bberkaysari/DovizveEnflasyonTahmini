0eb84406f65f4a76460dc94c3a138f468b3b0cf2
bberkaysari <bberkaysari0@gmail.com>
2025-05-04 02:59:29 +0300

    her sabah 7 de json güncelleme

---
+++ update_forecast.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Ortamı yükle
+source /Users/berkaysari/Desktop/dovizveenflasyon/.venv/bin/activate
+cd /Users/berkaysari/Desktop/dovizveenflasyon
+
+# JSON dosyalarını güncelle
+python3 train_model.py
+
+# Git ayarları
+git config user.name "bberkaysari"
+git config user.email "bberkaysari0@gmail.com"
+
+# Değişiklikleri Git'e ekle, commit et ve pushla
+git add tahmin.json enflasyon_tahmin.json
+git commit -m "Otomatik güncelleme: $(date '+%Y-%m-%d %H:%M:%S')"
+git push origin
